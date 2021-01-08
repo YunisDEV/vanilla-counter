11f61ecf9c43f40303f126f5070fc0378455a78b
Yunis Huseynzade <yunisdev.04@gmail.com>
2021-01-08 11:09:02 +0400

    README and default values

--- vanilla-counter.js
+++ vanilla-counter.js
@@ -2,17 +2,23 @@ function VanillaCounter() {
     let elements = document.querySelectorAll('[data-vanilla-counter]')
     elements.forEach(i => {
         let data = {
-            startAt: parseInt(i.getAttribute('data-start-at')) || 0,
-            endAt: parseInt(i.getAttribute('data-end-at')) || 100,
+            startAt: parseInt(i.getAttribute('data-start-at')),
+            endAt: parseInt(i.getAttribute('data-end-at')),
             delay: parseInt(i.getAttribute('data-delay')) || 0,
             format: i.getAttribute('data-format') || '{}',
-            time: parseInt(i.getAttribute('data-time')) || 500
+            time: parseInt(i.getAttribute('data-time')) || 1000
+        }
+        if (!data.startAt) {
+            throw new Error('data-start-at attribute is required')
+        }
+        if (!data.endAt) {
+            throw new Error('data-end-at attribute is required')
         }
         var counter = data.startAt
         i.innerHTML = data.format.replace('{}', counter)
         var intervalTime = Math.ceil(data.time / (data.endAt - data.startAt))
         setTimeout(() => {
-            var interval = setInterval(intervalHandler, data.time / (data.endAt - data.startAt))
+            var interval = setInterval(intervalHandler, intervalTime)
             function intervalHandler() {
                 counter += (data.endAt - data.startAt) / Math.abs(data.endAt - data.startAt) * 1
                 i.innerHTML = data.format.replace('{}', counter)
